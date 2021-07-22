9121741f0ed182a08394bbcece1c3650fcd32a74
MasterAnakin <mbm4816@gmail.com>
2021-07-22 16:39:16 +0200

    gf-fix

---
+++ gravity-forms.js
@@ -0,0 +1,10 @@
+//add this to gf HTML block
+<script type="text/javascript">
+    gform.addFilter( 'gform_product_total', function(total, formId){
+        //only apply logic to form ID 1
+        if(formId != 4)
+            return total;
+        if(total == 0)
+ jQuery("#field_4_7").hide();
+            return total;
+    } );</script>
