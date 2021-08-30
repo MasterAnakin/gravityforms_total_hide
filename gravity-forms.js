47c979833da8a9431864aebf68fbdea205fa4150
MasterAnakin <mbm4816@gmail.com>
2021-08-30 13:23:55 +0200

    Update gravity-forms.js

--- gravity-forms.js
+++ gravity-forms.js
@@ -1,7 +1,7 @@
 //add this to gf HTML block
 <script type="text/javascript">
     gform.addFilter( 'gform_product_total', function(total, formId){
-        //only apply logic to form ID 1
+        //only apply logic to form ID
         if(formId != 4)
             return total;
         if(total == 0)
